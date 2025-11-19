a2c28f07c266c8a0ac17584afbb6374ec410e32b
taeyoung0524 <miseoseo1@naver.com>
2025-11-19 14:36:55 +0900

    FEAT : 대략적인 구조 배치 완료

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,16 +1,12 @@
-import Footer from "../components/common/Footer"
-import Nav from "../components/common/Nav"
 import { Outlet } from "react-router-dom"
 import "../assets/scss/section/common/_main.scss"
 
 const RootLayout = () => {
   return (
     <div className="body">
-      <Nav/>
       <main className="page-container">
         <Outlet/> 
       </main>
-      <Footer/>
     </div>
   )
 }
--- src/pages/Center.jsx
+++ src/pages/Center.jsx
@@ -4,7 +4,21 @@ import "../assets/scss/section/section2/_center.scss"
 const Center = () => {
   return (
     <div className='center'>
-      센터지롱
+      <div className="container1">
+        <div className="title">DailyFrame</div>
+        <div className="button_containter">
+          <div className="image_button">이미지 업로드</div>
+          <div className="post_createbutton">포스터 생성하기</div>
+        </div>
+      </div>
+      <div className="direction">
+        →
+      </div>
+      <div className="container2">
+        <div className="post_box">
+          
+        </div>
+      </div>
     </div>
   )
 }
